8495820bc195e31f9ed75024239d7d8491acbd88
Jay742009 <72810212+Jay742009@users.noreply.github.com>
2021-05-06 20:48:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preloaded() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function takesnapshot() {
+    save('mypic.png');
+}
